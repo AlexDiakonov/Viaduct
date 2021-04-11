db90759b4bc7c23793adcd66ff8a67f218a31d69
AxDiv <imalex.diakonov@gmail.com>
2021-04-11 23:40:51 +0300

    about us + long read + contact us

---
+++ scripts/aboutUsOpener.js
@@ -0,0 +1,33 @@
+const benefits = document.querySelectorAll(".benefits_list_item");
+benefits.forEach((benefit) => {
+  benefit.addEventListener("click", (e) => {
+    if (e.currentTarget.classList.contains("active")) {
+      e.currentTarget.classList.remove("active");
+      benefits.forEach((ben) => {
+        ben.classList.remove("active");
+      });
+    } else {
+      benefits.forEach((ben) => {
+        ben.classList.remove("active");
+      });
+      e.currentTarget.classList.add("active");
+    }
+  });
+});
+
+const faq = document.querySelectorAll(".faq_list_items");
+faq.forEach((f) => {
+  f.addEventListener("click", (e) => {
+    if (e.currentTarget.classList.contains("active")) {
+      e.currentTarget.classList.remove("active");
+      faq.forEach((fa) => {
+        fa.classList.remove("active");
+      });
+    } else {
+      faq.forEach((fa) => {
+        fa.classList.remove("active");
+      });
+      e.currentTarget.classList.add("active");
+    }
+  });
+});
--- scripts/dropdown_menu.js
+++ scripts/dropdown_menu.js
@@ -1,7 +1,6 @@
 const menuBtn = document.querySelector(".nav_btn");
 const menuContainer = document.querySelector(".dropdown_menu");
-console.log("menuBtn");
-menuBtn.addEventListener("click", (ev) => {
+menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("open_menu");
   menuContainer.classList.toggle("open_dropdown");
 });
