0448efcf8dbe97e10ec19d34d1b7e646749888f2
AlexDiakonov <imalex.diakonov@gmail.com>
2021-04-12 11:52:15 +0300

    Version of Vlad

--- scripts/dropdown_menu.js
+++ scripts/dropdown_menu.js
@@ -1,6 +1,7 @@
 const menuBtn = document.querySelector(".nav_btn");
 const menuContainer = document.querySelector(".dropdown_menu");
-menuBtn.addEventListener("click", () => {
+console.log("menuBtn");
+menuBtn.addEventListener("click", (ev) => {
   menuBtn.classList.toggle("open_menu");
   menuContainer.classList.toggle("open_dropdown");
 });
