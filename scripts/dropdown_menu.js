a14e5f12a96ecc7b7f7122ae4b1ae1a15feac926
AxDiv <imalex.diakonov@gmail.com>
2021-04-22 18:33:42 +0300

    Menu fix

--- scripts/dropdown_menu.js
+++ scripts/dropdown_menu.js
@@ -3,4 +3,23 @@ const menuContainer = document.querySelector(".dropdown_menu");
 menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("open_menu");
   menuContainer.classList.toggle("open_dropdown");
+  setTimeout(function () {
+    menuContainer.classList.toggle("opened_dropdown");
+  }, 300);
+});
+
+document.addEventListener("click", (e) => {
+  if (menuContainer.classList.contains("opened_dropdown")) {
+    if (
+      e.target !== menuContainer &&
+      menuContainer.contains(e.target) == false
+    ) {
+      console.log("sdsdsd");
+      console.log(menuContainer.childNodes[1]);
+
+      menuBtn.classList.remove("open_menu");
+      menuContainer.classList.remove("open_dropdown");
+      menuContainer.classList.remove("opened_dropdown");
+    }
+  }
 });
